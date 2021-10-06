8faef997836dddfd97dc8f11f556bbd18e002705
Ebru E. Güngör <ebruekici@gmail.com>
2021-10-06 13:09:35 +0300

    initial commit

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,32 @@
+﻿
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB; database=Dbmvccategories;" +
+                "integrated security=true;");
+        }
+
+        //context sınıfı içindeki propertyler birere tabloya karşılık
+        //About projedeki sınıf, Abouts tablo 
+        public DbSet<About> Abouts  { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        [Key]
+        public int CatID { get; set; }
+
+        [StringLength(50)]
+        public string CatName { get; set; }
+
+        [StringLength(200)]
+        public string CatDescription { get; set; }
+        public bool CatStatus { get; set; }
+
+        //ilişki
+        public ICollection<Heading> Headings { get; set; }
+    }
+}
